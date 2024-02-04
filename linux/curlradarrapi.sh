e49f09e253aa7017ba78b7796e4237e0180a520e
marknizzle84 <marknizzle84@gmail.com>
2024-02-04 23:56:30 +0000

    curl radarr api

---
+++ linux/curlradarrapi.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+#titles of unwatched movies query with jq - "./curlplexapi.sh -q library/sections/1/unwatched |jq -r '.[].Metadata[].title' "
+#titles of unwatched tvshow query with jq - "./curlplexapi.sh -q library/sections/2/unwatched |jq -r '.[].Metadata[].title' "
+# select item 3 in array with jq - " ./curlplexapi.sh -q library/sections/1/unwatched |jq -r '.[].Metadata[2]' "
+# give details of all movies between years - "./curlplexapi.sh -q library/sections/1/unwatched |jq -r '.[].Metadata[]| select(.year >= 1935 and .year <= 1969)' "
+# give just titles of all movies between years - "./curlplexapi.sh -q library/sections/1/unwatched |jq -r '.[].Metadata[]| select(.year >= 1935 and .year <= 1969)| .title' "
+# query for video codecs not h264 and return results " ./curlplexapi.sh -q library/sections/1/unwatched | jq -r '.[].Metadata[] | select(.Media[].videoCodec != "h264") | .title,.Media[].videoCodec' "
+# query out to table - " ./curlplexapi.sh -q library/sections/1/unwatched | jq -r '.[].Metadata[] | select(.Media[].videoCodec == "vc1") | "\(.title) \(.Media[].videoCodec)"' | column -t -s $'\t' "
+
+
+while getopts "q:" flag
+do
+    case "${flag}" in
+        q) query=${OPTARG};;
+    esac
+done
+
+baseurl="http://192.168.1.62:7878"
+token=$(cat radarrtoken.pem)
+#echo "$baseurl/$query?apikey=$token"
+
+curl -X GET "$baseurl$query?apikey=$token" \
+  -H 'accept: application/json' \
+  -H "X-Api-Key: $token"
