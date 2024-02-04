618a09d6feefb82f290abe91af8bd0cf16aaf61c
marknizzle84 <marknizzle84@gmail.com>
2024-02-04 20:29:54 +0000

    jq examples

--- linux/curlplexapi.sh
+++ linux/curlplexapi.sh
@@ -4,7 +4,7 @@
 # select item 3 in array with jq - " ./curlplexapi.sh -q library/sections/1/unwatched |jq -r '.[].Metadata[2]' "
 # give details of all movies between years - "./curlplexapi.sh -q library/sections/1/unwatched |jq -r '.[].Metadata[]| select(.year >= 1935 and .year <= 1969)' "
 # give just titles of all movies between years - "./curlplexapi.sh -q library/sections/1/unwatched |jq -r '.[].Metadata[]| select(.year >= 1935 and .year <= 1969)| .title' "
-
+# query for video codecs not h264 and return results " ./curlplexapi.sh -q library/sections/1/unwatched | jq -r '.[].Metadata[] | select(.Media[].videoCodec != "h264") | .title,.Media[].videoCodec' "
 while getopts "q:" flag
 do
     case "${flag}" in
