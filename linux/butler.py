66248a61d4b7dad8da399a4a54cf4f4103cdbf6d
marknizzle84 <marknizzle84@gmail.com>
2024-02-04 19:32:01 +0000

    token alignment support

--- linux/butler.py
+++ linux/butler.py
@@ -1,11 +1,13 @@
 import requests
 
 url = "http://192.168.1.61:32400/butler"
+with open('plextoken.pem', 'r') as file:
+    token = file.read()
 
 payload={}
 headers = {
   'Accept': 'application/json',
-  'X-Plex-Token': 'whUWzw2_CHyxJkMx76Mj'
+  'X-Plex-Token': token
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
--- linux/unwatched.py
+++ linux/unwatched.py
@@ -2,12 +2,12 @@ import requests
 
 url = "http://192.168.1.61:32400/library/sections/2/unwatched"
 with open('plextoken.pem', 'r') as file:
-    my_token = file.read()
+    token = file.read()
 
 payload={}
 headers = {
   'Accept': 'application/json',
-  'X-Plex-Token': my_token
+  'X-Plex-Token': token
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
